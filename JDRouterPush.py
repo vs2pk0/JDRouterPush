aa324ea011b8f396a804c02536e71cfbad5fbdda
vs2pk0 <904239787@qq.com>
2021-03-02 17:24:29 +0800

    Update JDRouterPush.py

--- JDRouterPush.py
+++ JDRouterPush.py
@@ -87,9 +87,9 @@ def todayPointDetail(headers):
             todayPointIncome = info["todayPointIncome"]
             allPointIncome = info["allPointIncome"]
             content = content + "\n京东云无线宝_" + str(mac[-4:]) + ": " + str(todayPointIncome) + " 总积分: " + str(allPointIncome) \
-            # content = content + "\n  * 京东云无线宝_" + str(mac[-4:]) + " ==>\n今日收益: " + str(todayPointIncome) + " 总积分: " + str(allPointIncome) \
                       + routerAccountInfo(headers, mac) \
-                      + pointOperateRecordsShow(headers,mac)
+                      + pointOperateRecordsShow(headers, mac)
+                        # content = content + "\n  * 京东云无线宝_" + str(mac[-4:]) + " ==>\n今日收益: " + str(todayPointIncome) + " 总积分: " + str(allPointIncome) \
     else:
         print("Request todayPointDetail failed!")
     return content

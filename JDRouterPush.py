02dd1f5d9239bbfe85c2a2a09b4854f09201d534
vs2pk0 <904239787@qq.com>
2021-03-02 17:11:51 +0800

    Update JDRouterPush.py

--- JDRouterPush.py
+++ JDRouterPush.py
@@ -37,13 +37,13 @@ def routerAccountInfo(headers,mac):
         "mac" : mac,
     }
     res = requests.get(jd_base_url + "routerAccountInfo", params=params, headers=headers)
-    content = {}
+    content = ""
     if res.status_code == 200:
         res_json = res.json()
         result = res_json["result"]
         accountInfo = result["accountInfo"]
         amount = accountInfo["amount"]
-        content["可用积分"] = str(amount) + routerActivityInfo(headers,mac)
+        content = " 可用积分:" + str(amount) + routerActivityInfo(headers,mac)
     else:
         print("获取routerAccountInfo失败")
     return content
@@ -52,14 +52,13 @@ def routerActivityInfo(headers,mac):
     params = {
         "mac": mac,
     }
-    content = {}
+    content = ""
     res = requests.get(jd_base_url + "router:activityInfo", params=params, headers=headers)
     if res.status_code == 200:
         res_json = res.json()
         result = res_json["result"]
         satisfiedTimes = result["routerUnderwayResult"]["satisfiedTimes"]
-        content["累计在线"] = str(satisfiedTimes)+"天"
-
+        content = "    累计在线:" + str(satisfiedTimes)+"天"
     else:
         print("获取routerActivityInfo失败")
     return content
@@ -71,10 +70,7 @@ def todayPointDetail(headers):
         "pageSize": "30",
         "currentPage": "1",
     }
-    content = {
-        "数据日期": "",
-        "总可用积分":""
-    }
+    content = ""
     MAC = []
     res = requests.get(jd_base_url + "todayPointDetail", params=params, headers=headers)
     if res.status_code == 200:
@@ -83,19 +79,15 @@ def todayPointDetail(headers):
         todayDate = result["todayDate"]
         totalRecord = result["pageInfo"]["totalRecord"]
         pointInfos = result["pointInfos"]
-        content["数据日期"] = todayDate
-        content["总可用积分"] = str(pinTotalAvailPoint(headers))
-        content["设备总数"] = str(totalRecord)
-        content["设备收益如下"] = ""
+        content = content + "数据日期: " + todayDate + "\n* 总可用积分: " + str(pinTotalAvailPoint(headers)) + "\n* 设备总数: " + str(totalRecord) + "\n* 设备收益如下:"
         for info in pointInfos:
             mac = info["mac"]
             MAC.append(mac)
             todayPointIncome = info["todayPointIncome"]
             allPointIncome = info["allPointIncome"]
-            content["京东云无线宝_" + str(mac[-4:]) + " ==>今日收益"] = str(todayPointIncome)
-            # content["总积分"] = str(allPointIncome) \
-            #           + routerAccountInfo(headers, mac) \
-            #           + pointOperateRecordsShow(headers,mac)
+            content = content + "\n  * 京东云无线宝_" + str(mac[-4:]) + " ==>\n今日收益: " + str(todayPointIncome) + " 总积分: " + str(allPointIncome) \
+                      + routerAccountInfo(headers, mac) \
+                      + pointOperateRecordsShow(headers,mac)
     else:
         print("Request todayPointDetail failed!")
     return content
@@ -107,13 +99,13 @@ def pointOperateRecordsShow(headers,mac):
         "pageSize": 7,
         "currentPage": 1
     }
-    content = {}
+    content = ""
     res = requests.get(jd_base_url + "pointOperateRecords:show", params=params, headers=headers)
     if res.status_code == 200:
         res_json = res.json()
         result = res_json["result"]
         pointRecords = result["pointRecords"]
-        content["最近7条记录"] = ""
+        content = "\n* 最近7条记录:"
         for pointRecord in pointRecords:
             recordType_str = " "
             recordType = pointRecord["recordType"]
@@ -124,7 +116,7 @@ def pointOperateRecordsShow(headers,mac):
                 recordType_str = "积分收入:"
             else:
                 recordType_str = "积分支出:"
-            content[str(createTime_str) + "\t" + recordType_str] = str(pointAmount)
+            content = content + "\n *  " + str(createTime_str) + "\t" + recordType_str + " " + str(pointAmount)
     else:
         print("获取pointOperateRecordsShow失败")
     return content
@@ -138,8 +130,7 @@ def sendNotification(SERVERPUSHKEY,text,desp):
     params = {
         "token":SERVERPUSHKEY,
         "title":text,
-        "content":desp,
-        "template":"json"
+        "content":desp
         #"text" : text,
         #"desp" : desp
     }
